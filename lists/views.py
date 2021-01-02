d62dc28b21434f5e0242688547f4597efd5bc89a
Sergey Kipnis <sergey.kipnis@tribe29.com>
2021-01-02 22:56:07 +0100

    refactored to use template

--- lists/views.py
+++ lists/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Создайте ваши представления здесь.
 def home_page(request):
     """домашняя страница"""
-    return HttpResponse("<html><title>To-Do lists</title></html>")
+    return render(request, 'home.html')
